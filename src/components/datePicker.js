3ce6515f9e81053b97186b29a935f9b6a3ce36a0
Yuyu Zhang <1204891354@qq.com>
2023-01-21 17:43:05 +0800

    feat: add mouseOver and mouseLeave event for calendar

--- src/components/datePicker.js
+++ src/components/datePicker.js
@@ -1,5 +1,6 @@
 import '../styles/icon.css';
-import Icons from '../utils/icons';
+import '../styles/datePicker.css';
+import { useState } from 'react';
 
 export default function DatePicker() {
   const defaultDate = new Date();
@@ -7,10 +8,22 @@ export default function DatePicker() {
   const month = defaultDate.getMonth() + 1;
   const date = defaultDate.getDate();
   const DateRender = `${year}-${month}-${date}`;
+  const [isClose, setIsClose] = useState(false);
+
+  function handleOver() {
+    setIsClose(true);
+  }
+
+  function handleLeave() {
+    setIsClose(false);
+  }
+
   return (
-    <header>
-      <input value={DateRender} />
-      <Icons iconName="#icon-calendar" />
+    <header className="head-calendar">
+      <label>{DateRender}</label>
+      <svg className="icon" aria-hidden="true" onMouseOver={handleOver} onMouseLeave={handleLeave}>
+        <use xlinkHref={isClose ? '#icon-close' : '#icon-calendar'} />
+      </svg>
     </header>
   );
 }
