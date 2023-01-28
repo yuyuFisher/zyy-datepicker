133bd070259c03fe25d988fc8b6b0edc62a3cd63
Yuyu Zhang <1204891354@qq.com>
2023-01-28 17:22:49 +0800

    fix: change import ReactDom/Client to ReactDom

--- src/components/Popup/popup.js
+++ src/components/Popup/popup.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 export default function Popup(props) {
-  const { visible, getPopupPosition } = props;
+  const { visible, getPopupPosition, children } = props;
   const [pos, setPos] = useState({ left: 0, top: 0 });
   const { left, top } = pos;
 
@@ -15,7 +15,9 @@ export default function Popup(props) {
     <div
       className="date-popup"
       style={{ left, top, display: visible ? 'block' : 'none' }}
-    />,
+    >
+      {children}
+    </div>,
     document.body,
   );
 }
