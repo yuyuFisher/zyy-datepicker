098394fe1340b982190a8590f5224e1b5a4705a4
Yuyu Zhang <1204891354@qq.com>
2023-01-29 21:21:23 +0800

    feat: add panel head of '《 < date > 》'

--- src/components/Panel/panel.js
+++ src/components/Panel/panel.js
@@ -1,1 +1,72 @@
-export default function Panel() {}
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import {
+  skipYears,
+  skipMonths,
+} from '../../utils/util';
+import './pannel.css';
+
+const now = new Date();
+
+export default function Panel(props) {
+  const { id, value } = props;
+  const [date, setDate] = useState(value || now);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+
+  useEffect(() => {
+    setDate(value || now);
+  }, [value]);
+
+  const jumpTo = (type, total) => {
+    let newDate;
+
+    if (type === 'year') {
+      newDate = skipYears(date, total);
+    } else if (type === 'month') {
+      newDate = skipMonths(date, total);
+    }
+    setDate(newDate);
+  };
+
+  return (
+    <div className="date-panel" id={id}>
+      <div className="date-panel-header">
+        <span className="icons">
+          <span
+            className="iconfont icon-arrow-double-left"
+            onClick={() => jumpTo('year', -1)}
+          />
+          <span
+            className="iconfont icon-arrow-left-bold"
+            onClick={() => jumpTo('month', -1)}
+          />
+        </span>
+        <div className="date-panel-header-center">
+          {year}
+          {' '}
+          年
+          {' '}
+          {month}
+          {' '}
+          月
+        </div>
+        <span className="icons">
+          <span
+            className="iconfont icon-arrow-right-bold"
+            onClick={() => jumpTo('month', 1)}
+          />
+          <span
+            className="iconfont icon-arrow-double-right"
+            onClick={() => jumpTo('year', 1)}
+          />
+        </span>
+      </div>
+    </div>
+  );
+}
+
+Panel.propTypes = {
+  id: PropTypes.string,
+  value: PropTypes.instanceOf(Date),
+};
