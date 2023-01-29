f436c7d606436f682ea00e4c3c78c3818599128b
Yuyu Zhang <1204891354@qq.com>
2023-01-29 21:23:11 +0800

    feat: add date-panel-th

--- src/components/Panel/panel.js
+++ src/components/Panel/panel.js
@@ -62,6 +62,15 @@ export default function Panel(props) {
           />
         </span>
       </div>
+      <div className="date-panel-th">
+        <div className="date-panel-th-item date-panel-th-weekend">日</div>
+        <div className="date-panel-th-item">一</div>
+        <div className="date-panel-th-item">二</div>
+        <div className="date-panel-th-item">三</div>
+        <div className="date-panel-th-item">四</div>
+        <div className="date-panel-th-item">五</div>
+        <div className="date-panel-th-item date-panel-th-weekend">六</div>
+      </div>
     </div>
   );
 }
