095d35885fc7acca3a462881b6446f4460a4700d
Yuyu Zhang <1204891354@qq.com>
2023-01-29 17:16:06 +0800

    feat: add function to get month start and last date

--- src/utils/util.js
+++ src/utils/util.js
@@ -51,6 +51,20 @@ const skipDates = (date, days) => {
   return result;
 };
 
+function getMonthAllDays(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  return new Date(year, month, 0).getDate();
+}
+
+function getMonthStartAndLastDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const firstDate = new Date(year, month, 1);
+  const lastDate = new Date(year, month, getMonthAllDays(date));
+  return [firstDate, lastDate];
+}
+
 export {
   formatDate,
   uuid,
