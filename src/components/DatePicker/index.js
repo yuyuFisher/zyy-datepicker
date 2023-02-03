0026986da8f2042e453f9d1f2dc8880d03791daf
Yuyu Zhang <1204891354@qq.com>
2023-02-03 12:09:19 +0800

    refactor: Use index.js instead of datePicker.js, popup.js and panel.js. Delete noderef in CSSTransition component and delete strict mode upper <App /> component

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -1,3 +1,62 @@
-import DatePicker from './datePicker';
+import {
+  useEffect, useMemo, useRef, useState,
+} from 'react';
+import PropTypes from 'prop-types';
+import formatDate from '../utils/formatDate';
+import DateInputBox from './dateInputBox';
+import Popup from '../Popup';
+import Panel from '../Panel';
+import './datePicker.css';
 
-export default DatePicker;
+export default function DatePicker(props) {
+  const {
+    defaultValue = new Date(),
+    onChange,
+  } = props;
+  const [dateValue, setDateValue] = useState(defaultValue);
+  const [popupVisible, setPopupVisible] = useState(false);
+  const rootRef = useRef(null);
+  const dateString = useMemo(
+    () => (dateValue ? formatDate(dateValue) : ''),
+    [dateValue],
+  );
+
+  useEffect(() => {
+    onChange?.(dateValue);
+  }, [dateValue]);
+
+  const handleClear = (e) => {
+    setDateValue(null);
+    setPopupVisible(false);
+    e.stopPropagation();
+  };
+
+  const handleDatepickerClick = () => {
+    setPopupVisible(true);
+  };
+
+  const onPanelChange = (date) => {
+    setDateValue(date);
+    setTimeout(() => { // 选完弹框立刻消失
+      setPopupVisible(false);
+    }, 0);
+  };
+
+  return (
+    <section className="head" ref={rootRef} onClick={handleDatepickerClick}>
+      <DateInputBox dateValue={dateString} onClose={handleClear} />
+      <Popup
+        visible={popupVisible}
+        target={rootRef}
+        onVisible={(visible) => setPopupVisible(visible)}
+      >
+        <Panel dateValue={dateValue} onChange={onPanelChange} />
+      </Popup>
+    </section>
+  );
+}
+
+DatePicker.propTypes = {
+  defaultValue: PropTypes.instanceOf(Date),
+  onChange: PropTypes.func,
+};
