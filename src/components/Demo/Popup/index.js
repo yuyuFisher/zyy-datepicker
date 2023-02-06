69b69d15eff6613fde26b42b25affb29a06adc59
Yuyu Zhang <1204891354@qq.com>
2023-02-06 16:07:22 +0800

    refactor: add popup demo in app.js

---
+++ src/components/Demo/Popup/index.js
@@ -0,0 +1,20 @@
+import { useRef, useState } from 'react';
+import Popup from '../../Popup';
+
+export default function PopupDemo() {
+  const [visible, setVisible] = useState(false);
+  const buttonRef = useRef();
+
+  return (
+    <div>
+      <button type="button" ref={buttonRef} onClick={() => setVisible(true)}>trigger popup</button>
+      <Popup
+        visible={visible}
+        targetRef={buttonRef}
+        onVisibilityChange={(isVisible) => setVisible(isVisible)}
+      >
+        I am a popup
+      </Popup>
+    </div>
+  );
+}
